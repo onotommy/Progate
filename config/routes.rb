44e6d15169343dd84dd9d10daceac7771bfc292a
onotommy <o5i1.bigifield@gmail.com>
2018-09-27 22:31:45 +0000

    “tweet_app”

--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,7 @@ Rails.application.routes.draw do
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
+  get "users/:id/likes" => "users#likes"
   
   get "posts/index" => "posts#index"
   get "posts/new" => "posts#new"
